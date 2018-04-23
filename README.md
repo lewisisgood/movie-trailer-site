23348d5ac930eebf9bc978d48d979358215e845b
circle <circle@local>
2018-04-23 15:36:44 -0700

    added docstrings, a better readme, and PEP8 compliance

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
-# My Favorite Movies
+# My Favorite Movies Website
 Source code for a Movie Trailer website.
 
-# Instructions:
-1. Clone my repo
-2. Ensure Python is installed "python --version"
-3. Run with "python entertainment_center.py" to generate my movie trailer website. 
+This project is used to create a website that list my favorite movies along with their posters. When a user clicks on one of the posters, a trailer of the movie from Youtube plays in the same window.
+
+This project is forked from Udacity's ud036_StarterCode. I implemented functionality in Python that generates a series of "Movie" classes, which are then used in launching a browser window with the newly created HTML websire.
+
+
+### Prerequesites:
+Ensure that Python 2.7+ and git are installed:
+```
+python --version
+git --version
+```
+
+
+### Installing
+
+From the Terminal, close this repo onto your computer with:
+
+```
+git clone https://github.com/lewisisgood/ud036_StarterCode
+```
+
+Move into the new directory:
+
+```
+cd ud036_StarterCode/
+```
+
+Run the entertainment_center.py to generate the movie trailer website:
+
+```
+python entertainment_center.py
+```
+
+This will create an HMTL file called "fresh_tomatoes.html" and launch a browser displaying this new website.
+
+
+## Built With
+
+* Python 2.7.14
+* git 2.14.1
+
+## Authors
+
+* **Lewis King** - *Initial work* - [Github](https://github.com/lewisisgood)
+
+## Acknowledgments
+
+* Thanks to Udacity for the starter code!
--- media.py
+++ media.py
@@ -1,14 +1,57 @@
-import webbrowser 
+import webbrowser
+
 
 class Movie(object):
-	"""Creates objects that contains critical information about a movie, including a trailer"""
-	#Constructor
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-	    self.title = movie_title 
-	    self.storyline = movie_storyline
-	    self.poster_image_url = poster_image
-	    self.trailer_youtube_url = trailer_youtube
+    """
+    Creates an object that represents a movie
 
-	#This function opens a movie trailer as a container in the same web page (rather than new tab)
-	def show_trailer(self):
-	    webbrowser.open(self.trailer_youtube_url)
+    Object contains critical information about a movie for a trailer website,
+    including the title, storyline, poster image URL, and Youtube trailer URL.
+
+    Also included is functionality to open the Youtube trailer within the
+    same window.
+
+    Parameters
+    ----------
+    movie_title : str
+        The title of the movie
+    movie_storyline : str
+        A quick summary of the storyline of the movie
+    poster_image : str
+        URL to Wikimedia's image on file
+    trailer_youtube : str
+        URL to Youtube's trailer video for the movie
+
+    Attributes
+    ----------
+    title : str
+        The title of the movie
+    storyline : str
+        A quick summary of the storyline of the movie
+    poster_image_url : str
+        URL to Wikimedia's image on file
+    trailer_youtube_url : str
+        URL to Youtube's trailer video for the movie
+    """
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """
+        Opens a movie trailer as a container
+        in the same web page (rather than new tab)
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        webbrowser.open(self.trailer_youtube_url)
